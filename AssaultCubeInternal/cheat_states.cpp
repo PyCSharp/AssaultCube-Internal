28e9f0bd1eda4c7e08f1ca51a113571f531f5b66
PyCSharp <210414190+PyCSharp@users.noreply.github.com>
2026-02-08 14:45:43 +0100

    Fix include statement and formatting in cheat_states.cpp

--- AssaultCubeInternal/cheat_states.cpp
+++ AssaultCubeInternal/cheat_states.cpp
@@ -1,4 +1,4 @@
-#include <cheat_states.h>
+#include "cheat_states.h"
 
 bool infiniteHealthActiavted = true;
 bool infiniteHealthActivated = true;
@@ -10,3 +10,4 @@ bool knifeSpamActivated = true;
 bool noRecoilActivated = true;
 bool rapidFireActivated = true;
 bool grenadeSpamActivated = true;
+
